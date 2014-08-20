7420e627bc7f167ce4638ed23a58f4ee964ee775
Vladimir Lapikov <vl.lapikov@gmail.com>
2014-08-20 03:58:05 +0200

    more comments

--- src/Lapikov/HangmanBundle/Controller/DefaultController.php
+++ src/Lapikov/HangmanBundle/Controller/DefaultController.php
@@ -7,18 +7,28 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * http://symfony.com/doc/current/cookbook/routing/redirect_trailing_slash.html
- *
  * Class RedirectingController
  * @package Lapikov\HangmanBundle\Controller
  */
 class DefaultController extends Controller
 {
+    /**
+     * Default route
+     *
+     * @param Request $request
+     * @return Response
+     */
     public function welcomeAction(Request $request)
     {
         return new Response($this->container->get('jms_serializer')->serialize(array('message' => 'Welcome to Hangman! Please use API calls'), 'json'));
     }
 
+    /**
+     * http://symfony.com/doc/current/cookbook/routing/redirect_trailing_slash.html
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function removeTrailingSlashAction(Request $request)
     {
         $pathInfo = $request->getPathInfo();
